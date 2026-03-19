3cb477590e9a630a6a7ef635a804c5a02b0d7a83
jokertalor-cpu <jokertalor@gmail.com>
2026-03-19 18:33:45 +0630

    Add files via upload

--- searchscript.js
+++ searchscript.js
@@ -1,25 +1,21 @@
-function searchBooks() {
-  // ၁။ Input ထဲက စာသားကို ယူတယ်
-  let input = document.getElementById('searchinput').value.toLowerCase();
-  let ul = document.getElementById('booklist');
-  let li = ul.getElementsByTagName('li');
 
-  // ၂။ စာရိုက်ထားရင် List ကို ဖော်မယ်၊ ဘာမှမရှိရင် ပြန်ဖျောက်မယ်
-  if (input.length > 0) {
-    ul.style.display = "block";
-  } else {
-    ul.style.display = "none";
-  }
+        function searchBooks() {
+            let input = document.getElementById('searchinput').value.toUpperCase(); // စာလုံးအကြီးနဲ့စစ်မယ်
+            let ul = document.getElementById('booklist');
+            let li = ul.getElementsByTagName('li');
 
-  // ၃။ တစ်ခုချင်းစီကို လိုက်စစ်တယ်
-  for (let i = 0; i < li.length; i++) {
-    let textValue = li[i].textContent || li[i].innerText;
-    
-    // စာသားတူတာ ပါနေရင် ပြမယ်၊ မပါရင် ဖျောက်မယ်
-    if (textValue.toLowerCase().indexOf(input) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
+            if (input.length > 0) {
+                ul.style.display = "block";
+            } else {
+                ul.style.display = "none";
+            }
+
+            for (let i = 0; i < li.length; i++) {
+                let text = li[i].textContent || li[i].innerText;
+                if (text.toUpperCase().indexOf(input) > -1) {
+                    li[i].style.display = "";
+                } else {
+                    li[i].style.display = "none";
+                }
+            }
+        }
